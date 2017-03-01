a6c66ee5bbcb8954ca65a9334aac385f0cac28ef
Scott Rehlander <scott@stackwave.com>
2017-03-01 17:10:52 -0500

    Use export = {} syntax for config files

--- server/config/app.config.dev.ts
+++ server/config/app.config.dev.ts
@@ -1,11 +1,7 @@
-const config = {
+export = {
     "pbs": {
         "baseUrl": "https://cbnt-project-catalog-qa.broadinstitute.org/projectdb/api",
         "user": "PBS-CBIP-app",
         "pass": "VzO3KnHG"
     }
-}
-
-export function get() {
-    return config;
-}
+};
