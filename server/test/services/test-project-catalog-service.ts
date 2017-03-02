a98bb3b3e0453753a7a79efd7158baf807f2b2ad
Scott Rehlander <scoot@vpn6-73.broadinstitute.org>
2017-03-02 09:50:58 -0500

    Remove .ds_store.. not sure why it was there

--- server/test/services/test-project-catalog-service.ts
+++ server/test/services/test-project-catalog-service.ts
@@ -2,7 +2,7 @@ let assert = require('assert');
 
 import projectCatalogService = require('../../services/project-catalog-service');
 
-describe('GetProjectrs', function() {
+describe('ProjectCatalogTest', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
